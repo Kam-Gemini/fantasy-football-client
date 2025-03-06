132bf712215b24897d5a6363023135788cebfa0d
Kamran <kamran.raja@hotmail.co.uk>
2025-03-06 11:20:54 +0000

    bugs fixed

--- src/components/SelectTeam/Players.jsx
+++ src/components/SelectTeam/Players.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PlayerCard from './PlayerCard'
 import Filters from './Filters'
 import Spinner from '../Spinner/Spinner'
 import styles from './SelectTeam.module.css'
 
 const Players = ({ filterBy, setFilterBy, listAllClubs, isLoading, displayedPlayers, handleAddPlayer }) => {
+    const [pickedPlayers, setPickedPlayers] = useState([])
+
+    const handlePlayerClick = (player) => {
+        handleAddPlayer(player)
+        setPickedPlayers([...pickedPlayers, player.id])
+    }
+
     return (
         <div className={styles.rightColumn}>
             <Filters filterBy={filterBy} setFilterBy={setFilterBy} listAllClubs={listAllClubs}/>
             <div className={styles.playersContainer}>
                 { isLoading ? <Spinner /> : 
                     displayedPlayers.map(player => (
-                        <button key={player.id} onClick={() => handleAddPlayer(player)} className={styles.playerButton}>
+                        <button 
+                            key={player.id} 
+                            onClick={() => handlePlayerClick(player)} 
+                            className={styles.playerButton}
+                            disabled={pickedPlayers.includes(player.id)}
+                        >
                             <PlayerCard player={player} />
                         </button>
                     ))
--- src/components/SelectTeam/VacantPlayer.jsx
+++ src/components/SelectTeam/VacantPlayer.jsx
@@ -3,12 +3,13 @@ import styles from './SelectTeam.module.css'
 export default function VacantPlayer({ position }) {
 
     return (
-        <section className={styles.vacantCard}>
-            <img src="/assets/default.jpg" alt="player" className={styles.playerImage} />
-                <section className={styles.vacantPlayerInfo}>
-                    <p><strong>{position}</strong></p>
-                </section>
-        </section>
-              
+        <button className={styles.playerButton}>
+            <section className={styles.vacantCard}>
+                <img src="/assets/default.jpg" alt="player" className={styles.playerImage} />
+                    <section className={styles.vacantPlayerInfo}>
+                        <p><strong>{position}</strong></p>
+                    </section>
+            </section>
+        </button>     
     )
 }
